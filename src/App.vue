756d89b3790647ee7f1cf21d7b45ce5e3e3ee52a
lhan <lhan12345@hotmail.com>
2023-04-09 23:09:53 +0800

    准备提取模板和样式

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import axios from 'axios';
-import { ref, onMounted} from 'vue';
-import { Delete, Refresh, VideoPause, Lock, Pointer} from '@element-plus/icons-vue'
-import { ipcRenderer, remote} from "electron"
+import { ref, onMounted } from 'vue';
+import { Delete, Refresh, VideoPause, Lock, Pointer } from '@element-plus/icons-vue'
+import { ipcRenderer, remote } from "electron"
 import Markdown from 'markdown-it';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/monokai-sublime.css';
@@ -22,15 +22,15 @@ const md = Markdown({
 });
 
 
-let test_q = '<a href="file/temp/reference-1.txt" class="asklink" title="Open text snippet 0.218">[1] </a> 写一个vue的<script setup>示例,沙发上地方暗示法发啥呆发啥发水电费安抚沙发舒服asdf阿四饭店啥'
-let test_a = '好的    \n这里是一个简单的Python类\n> 它只有一个属性和一个方法来设置和打印\n> 该属性值:\n\n```python\nclass Person:\n    def __init__(self, name):\n        self.name = name\n    \n    def say_hello(self):\n        print(f"Hello, my name is {self.name}")\n```\n\n在这个例子中,我们定义了一个`Person`类,它具有一个`__init__`方法来设置`name`属性,以及一个`say_hello`方法来打印出该属性的值。\n\n我们可以使用以下代码创建一个`Person`对象,并使用`say_hello`方法打印出其名称:\n\n```python\nperson = Person("Alice")\nperson.say_hello()\n```\n\n这将产生以下输出:\n\n```\nHello, my name is Alice\n```\n\n'
+// let test_q = '<a href="file/temp/reference-1.txt" class="asklink" title="Open text snippet 0.218">[1] </a> 写一个vue的<script setup>示例,沙发上地方暗示法发啥呆发啥发水电费安抚沙发舒服asdf阿四饭店啥'
+// let test_a = '好的    \n这里是一个简单的Python类\n> 它只有一个属性和一个方法来设置和打印\n> 该属性值:\n\n```python\nclass Person:\n    def __init__(self, name):\n        self.name = name\n    \n    def say_hello(self):\n        print(f"Hello, my name is {self.name}")\n```\n\n在这个例子中,我们定义了一个`Person`类,它具有一个`__init__`方法来设置`name`属性,以及一个`say_hello`方法来打印出该属性的值。\n\n我们可以使用以下代码创建一个`Person`对象,并使用`say_hello`方法打印出其名称:\n\n```python\nperson = Person("Alice")\nperson.say_hello()\n```\n\n这将产生以下输出:\n\n```\nHello, my name is Alice\n```\n\n'
 
 
-test_a = md.render(test_a);
+// test_a = md.render(test_a);
 // test_q = md.render(test_q);
-let QAcontext = ref([[test_q, test_a],[test_q, test_a]]);
+// let QAcontext = ref([[test_q, test_a], [test_q, test_a]]);
 
-// let QAcontext = ref([]);
+let QAcontext = ref([]);
 let scrollbarRef = ref(null);
 let itemList = ref(['Item 1', 'Item 2', 'Item 3'])
 let caretPosition = ref({ left: 0, top: 0 })
@@ -42,7 +42,13 @@ let defaultBodyHeight = currentWindow.getSize()[1] - 280
 let bodyHeight = ref(`${defaultBodyHeight}px`)
 let intervalId = null
 const dragHandle = ref(null);
+const tags = ref([
+  { name: 'Tag 1', type: '' },
+  { name: 'Tag 2', type: 'success' },
+  { name: 'Tag 3', type: 'info' },
+  { name: 'Tag 4', type: 'warning' },
 
+])
 
 currentWindow.on('resize', () => {
   bodyHeight.value = `${currentWindow.getSize()[1] - 280}px`
@@ -55,7 +61,7 @@ const sendRequests = () => {
   // 发送总请求
   let question = inputText.value
   axios.post('http://127.0.0.1:7860/run/ask', {
-    data: [question, "", "", "evernote", "", "brainshell"]
+    data: [question, "", "", "default", "", "brainshell"]
   }).then(response => {
     QAcontext.value = response['data']['data'][0]
     md2html()
@@ -178,12 +184,12 @@ onMounted(() => {
 
 
 <style>
-
 .QAs {
   /* height: 650px; */
   margin: 15px;
 
 }
+
 .Q {
   text-align: left;
   vertical-align: middle;
@@ -245,28 +251,37 @@ pre {
   white-space: -o-pre-wrap;
   word-wrap: break-word;
 }
-.preQ{
+
+.preQ {
   font-size: medium;
 }
+
 #textArea {
   border-radius: 0%;
   /* border:1px solid gray; */
 }
-#drag-handle{
+
+#drag-handle {
   -webkit-app-region: drag;
 }
 
+.mx-1 {
+  margin-bottom: 5px;
+  margin-right: 5px;
+}
 </style>
 
 
 <template>
-  <el-row justify="center" align="bottom" class="QAs" ref="chatWindow" :style="{ height:bodyHeight }">
+  <el-row justify="center" align="bottom" class="QAs" ref="chatWindow" :style="{ height: bodyHeight }">
     <el-col :span="24" gutter="10">
       <el-scrollbar ref="scrollbarRef" :max-height="bodyHeight">
         <div class="grid-content QA" v-for="(round, index) in QAcontext" :key="index">
           <div>
             <!-- <div class="Q" v-html="round[0]"></div> -->
-            <div class="Q"><pre class="preQ">{{ round[0] }}</pre></div>
+            <div class="Q">
+              <pre class="preQ">{{ round[0] }}</pre>
+            </div>
           </div>
           <div>
             <div class="A" v-html="round[1]"></div>
@@ -274,7 +289,7 @@ pre {
         </div>
       </el-scrollbar>
     </el-col>
-  </el-row> 
+  </el-row>
   <el-footer>
     <div id="magicInput">
       <div ref="list" class="mTagList" :style="caretPosition" v-show="showList">
@@ -284,21 +299,26 @@ pre {
           </li>
         </ul>
       </div>
+
+      <el-tag v-for="tag in tags" :key="tag.name" class="mx-1" closable :type="tag.type" round=true effect="dark"
+        size="small">
+        {{ tag.name }}
+      </el-tag>
+
       <!-- 如果要shift+enter提交，设置@keydown.shift.enter.prevent -->
       <el-input id="textArea" v-model.lazy="inputText" @input="handleInput" @keydown.enter.exact.prevent="sendRequests"
-        type="textarea" ref="inputRef" clearable="true" maxlength="1000" show-word-limit="true"
-        placeholder="请输入内容" resize="none" :autosize="{ minRows: 2, maxRows: 5 }">
+        type="textarea" ref="inputRef" clearable="true" maxlength="1000" show-word-limit="true" placeholder="请输入内容"
+        resize="none" :autosize="{ minRows: 2, maxRows: 5 }">
       </el-input>
       <el-row class="toolbar">
         <el-button class="toolButton" :icon="Refresh" text="true" bg="true" circle @click="clearContext" />
-        <el-button class="toolButton" :icon="Delete" text="true" bg="true" circle/>
+        <el-button class="toolButton" :icon="Delete" text="true" bg="true" circle />
         <el-button class="toolButton" :icon="VideoPause" text="true" bg="true" circle />
         <el-button class="toolButton" :icon="Lock" text="true" bg="true" circle @click="lock" />
         <el-button id="drag-handle" :icon="Pointer" text="true" bg="true" circle />
       </el-row>
     </div>
   </el-footer>
-
 </template>
 
 ///////////////////////////////////////
